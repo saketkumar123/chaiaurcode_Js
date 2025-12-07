ae33a9ed1afd07bbe445e4f8e83adee760ea927f
saketkumar123 <saket.ulsi@gmail.com>
2025-12-07 16:03:36 +0000

    added 01_variables

---
+++ Js/01_basics/01_Variables.js
@@ -0,0 +1,20 @@
+const accountId = 12345
+let accountEmail = "saket@gmail.com"
+var accountPassword = 465564765
+accountCity = "GayaJi"
+let accountState;
+
+// accountId = 090909 //Not Allowed
+
+console.log(accountId);
+
+accountEmail = "Saket.ulsi@gmail.com"
+accountPassword = 098765098
+accountCity = "Bangalore"
+
+/*
+Prefer not to use var
+because of issue in block and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
