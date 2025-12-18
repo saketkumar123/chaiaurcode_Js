aaf4d223083406c910242711d1ae840ed989fd80
saketkumar123 <saket.ulsi@gmail.com>
2025-12-18 10:29:22 +0530

    commit project 6

---
+++ Js/07_projects/6-unlimitedColors/chaiaurcode.js
@@ -0,0 +1,30 @@
+const randomColor = function () {
+    let color = '#';
+    const hex = '0123456789ABCDEF'
+
+    for (let i = 0; i < 6; i++) {
+        color += hex[Math.floor(Math.random() * 16)]
+    }
+
+    return color;
+}
+
+let intervalId;
+const startChangingColor = function () {
+
+    if (!intervalId)
+        intervalId = setInterval(changeBgColor, 1000)
+
+    function changeBgColor() {
+        document.body.style.backgroundColor = randomColor();
+    }
+}
+
+const stopChangingColor = function () {
+    clearInterval(intervalId);
+    intervalId = null;
+}
+
+document.getElementById('start').addEventListener('click', startChangingColor)
+
+document.getElementById('stop').addEventListener('click', stopChangingColor)
